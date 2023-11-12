ae66f1fcffc275235288951f162479ec95afe21f
Danubiabuba <danubiaalves184@gmail.com>
2023-11-12 12:43:33 -0300

    Adicionando button limpar

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ import string
 
 ####################### cores ############################
 
-cor0= "#FFFFFF"
-cor1= "#333333"
-cor2= "#38576b"
-
+cor0= "#c0c0c0" #cinza#
+cor1= "#333333" #preta#
+cor2= "#38576b" #azul#
+cor4= "#ffffff"
 ######################## janela ##############################
 
 janela = Tk()
-janela.geometry('300x320')
+janela.geometry('300x360')
 janela.title('Converso de Moedas')
 janela.configure(bg=cor0)
 janela.resizable(width=False, height=False)
@@ -24,7 +24,7 @@ style.theme_use("clam")
 
 ########################### divisao da janela #############################
 
-Frame_cima = Frame(janela, width=300, height=60, padx=0,pady=0, bg=cor2, relief='flat')
+Frame_cima = Frame(janela, width=300, height=60, padx=0,pady=0, bg=cor0, relief='flat')
 Frame_cima.grid(row=0,column=0, columnspan=2)
 
 Frame_baixo = Frame(janela, width=300, height=260, padx=0,pady=5, bg=cor0, relief='flat')
@@ -36,6 +36,9 @@ def converter():
     moeda_para = combo_para.get()
     valor_entrada = valor.get()
 
+
+
+    
     url = requests.get('https://api.exchangerate-api.com/v4/latest/{}'.format(moeda_de))
     dados = json.loads(url.text)
     cambio= dados['rates'][moeda_para]
@@ -54,15 +57,18 @@ def converter():
         simbolo = 'R$'
         
         
-    moeda_equivalente = simbolo + "{:,.2}".format(resultado)
+
+    moeda_equivalente = simbolo + "{:.2f}".format(resultado)
     app_resultado["text"] = moeda_equivalente
     
 
+        
+
 ########################## configurando frame de cima ############################
-Frame_cima = Label(janela, text='Conversor de Moedas', relief='solid', anchor='center', font=('Arial 16 bold'), bg=cor2,fg=cor0, width=23, height=2)
+Frame_cima = Label(janela, text='Conversor de Moedas', relief='solid', anchor='center', font=('Arial 16 bold'), bg=cor2, fg=cor1, width=23, height=2)
 Frame_cima.place(x=0, y=0)
 
-app_resultado = Label(Frame_baixo, relief='solid',  anchor='center', font=('Ivy 15 bold'), bg=cor0,fg=cor1, width=15, height=2)
+app_resultado = Label(Frame_baixo, relief='solid',  anchor='center', font=('Ivy 15 bold'), bg=cor4,fg=cor1, width=15, height=2)
 app_resultado.place(x=50, y=10)
 
 moeda = ['AOA', 'BRL', 'EUR', 'INR', 'USD']
@@ -80,11 +86,21 @@ combo_para = ttk.Combobox(Frame_baixo, height=200, width=8, font=('Ivy 12 bold')
 combo_para.place(x=160, y=120)
 combo_para['value'] = (moeda)
 
-valor = Entry(Frame_baixo, width=22, justify='center', font=('Ivy 12 bold'), relief='solid')
+valor = Entry(Frame_baixo, width=23, justify='center', font=('Ivy 12 bold'), relief='solid')
 valor.place(x=50, y=155)
 
-botao = Button(Frame_baixo, command=converter, text='converter', width=19, padx=5, height=1, bg=cor2, fg=cor0, font=('Ivy 12 bold'), relief='raised', overrelief=RIDGE)
-botao.place(x=50, y=210)
+botao = Button(Frame_baixo, command=converter, text='converter', width=19, padx=5, height=1, bg=cor2, fg=cor1, font=('Ivy 12 bold'), relief='flat', overrelief=RIDGE)
+botao.place(x=49, y=183)
+
+def limpar():
+    combo_de.delete(0, END)
+    combo_para.delete(0, END)
+    valor.delete(0, END)
+    app_resultado["text"] = ''
+
+ 
+botao_limpar = Button(Frame_baixo, text='limpar', command=limpar, width=15, padx=10, bg=cor2, fg=cor1, relief='raised', highlightthickness=0, bd=0, font=('Ivy 10 bold'))
+botao_limpar.place(x=85, y=225)
 
 
 janela.mainloop()
